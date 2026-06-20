ba919688bf9b6e63cb4d556dd3986f79a272020d
Félix Clemente <Felix.cle@hotmail.com>
2026-06-20 18:05:22 +0100

    feat: initial project setup - LisAeroGest ASP.NET MVC .NET 8

---
+++ LisAeroGest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LisAeroGest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
